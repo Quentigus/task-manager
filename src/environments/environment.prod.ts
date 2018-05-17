4696439e052e62aa4ebc7786f4708a233235ec92
Quentin GOSSELIN <quentin.gosselin@gmail.com>
2018-05-17 22:11:14 +0200

    default angular seed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    api: '79D7E6EC-8D40-9517-FF1C-7D7E55445200',
+    app: '94A856A0-1E32-CE0D-FFFC-0D7EC278E500'
+  },
+  services: {
+    users: `https://api.backendless.com/${this.backend.app}/${this.backend.api}/services/users/`,
+    tasks: `https://api.backendless.com/${this.backend.app}/${this.backend.api}/services/tasks/`,
+    lists: `https://api.backendless.com/${this.backend.app}/${this.backend.api}/services/lists/`
+  }
 };
